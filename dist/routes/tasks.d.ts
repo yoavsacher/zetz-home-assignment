60e52ff46d05ba5050d9c562dad5240eb33de658
yoavsacher <133858475+yoavsacher@users.noreply.github.com>
2025-06-01 11:26:33 +0300

    Delete dist directory

--- dist/routes/tasks.d.ts
+++
@@ -1,4 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export declare const shutdownWorkerPool: () => Promise<void>;
-export default router;
-//# sourceMappingURL=tasks.d.ts.map
--- dist/worker/taskWorker.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=taskWorker.d.ts.map
